bc9ca192b3d513996b096780eb224041b364d143
David Gay <oddshocks@riseup.net>
2014-03-20 11:57:48 -0400

    Massive reorganization of art.

--- art/art.rst
+++ art/art.rst
@@ -1,4 +1,10 @@
 Art
 ===
 
-This folder contains art assets for badges
+This folder contains art assets for badges.
+
+`jenn` contains Jenn Kotler's original artwork.
+
+`fedora` contains artwork for Fedora badges.
+
+`fossbox` contains artwork for RIT Badges.
--- art/svg-to-png.sh
+++ art/svg-to-png.sh
@@ -6,4 +6,7 @@
 # need some tweaking in order to properly do things. If you're unsure,
 # just manually run this line in each of the src folders, like I did.
 
+# Update 2014-03-20: I've reorganized the art directory structure,
+# so make sure to tweak this script as appropriate. -- oddshocks
+
 for i in *; do inkscape $i --export-png=`echo $i | sed -e 's/svg$/png/'`; done
